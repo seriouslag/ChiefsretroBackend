0b70f65437a41d89fdcba3f524b139cbbc6ced38
landon.gavin <landon.gavin@gmail.com>
2018-03-03 17:48:43 -0500

    Changed method calls to use the static methods

--- src/main/java/com/chiefsretro/controllers/StripeController.java
+++ src/main/java/com/chiefsretro/controllers/StripeController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/stripe")
 public class StripeController {
 
-    StripeService stripeService = new StripeService();
-
     @PostMapping("/webhookEndpoint")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
@@ -27,67 +25,82 @@ public class StripeController {
             String chargeId = "";
             String message = null;
 
-            if (event.getType().equals("charge.succeeded")) {
-                chargeId = ((Charge) event.getData().getObject()).getId();
-                message = "Payment Successful";
-                status = "paid";
-            } else if (event.getType().equals("charge.failed")) {
-                chargeId = ((Charge) event.getData().getObject()).getId();
-                message = "Payment failed to process.";
-                status = "declined";
-            } else if (event.getType().equals("charge.refunded")) {
-                chargeId = ((Charge) event.getData().getObject()).getId();
-                message = "Payment was refunded.";
-                status = "refunded";
-            } else if (event.getType().equals("charge.pending")) {
-                chargeId = ((Charge) event.getData().getObject()).getId();
-                message = "Payment is pending.";
-                status = "pending";
-            } else if (event.getType().equals("charge.updated")) {
-                chargeId = ((Charge) event.getData().getObject()).getId();
-                message = "This payment was recently updated...";
-                status = "processing_updated";
-            } else if (event.getType().equals("charge.dispute.closed")) {
-                chargeId = ((Dispute) event.getData().getObject()).getCharge();
-                message = "This order was disputed and closed";
-                status = "disputed_closed";
-                stripeService.createDispute((Dispute) event.getData().getObject());
-            } else if (event.getType().equals("charge.dispute.opened")) {
-                chargeId = ((Dispute) event.getData().getObject()).getCharge();
-                message = "This order is disputed";
-                status = "disputed_open";
-                stripeService.createDispute((Dispute) event.getData().getObject());
-            } else if (event.getType().equals("charge.dispute.created")) {
-                chargeId = ((Dispute) event.getData().getObject()).getCharge();
-                message = "This order is disputed";
-                status = "disputed_created";
-                stripeService.createDispute((Dispute) event.getData().getObject());
-            } else if (event.getType().equals("charge.dispute.funds_reinstated")) {
-                chargeId = ((Dispute) event.getData().getObject()).getCharge();
-                message = "Payment Successful/Disputed/Closed";
-                status = "disputed_closed";
-                stripeService.createDispute((Dispute) event.getData().getObject());
-            } else if (event.getType().equals("charge.dispute.funds_withdrawn")) {
-                chargeId = ((Dispute) event.getData().getObject()).getCharge();
-                message = "Order cancelled/Payment returned";
-                status = "disputed_closed_lost";
-                stripeService.createDispute((Dispute) event.getData().getObject());
-            } else if (event.getType().equals("charge.dispute.updated")) {
-                chargeId = ((Dispute) event.getData().getObject()).getCharge();
-                message = "This order is disputed";
-                status = "disputed_updated";
-                stripeService.createDispute((Dispute) event.getData().getObject());
-            } else if (event.getType().equals("charge.refund.updated")) {
-                chargeId = ((Refund) event.getData().getObject()).getCharge();
-                message = "This order was refunded";
-                status = "refunded";
-                stripeService.createRefund((Refund) event.getData().getObject());
+            switch (event.getType()) {
+                case "charge.succeeded":
+                    chargeId = ((Charge) event.getData().getObject()).getId();
+                    message = "Payment Successful";
+                    status = "paid";
+                    break;
+                case "charge.failed":
+                    chargeId = ((Charge) event.getData().getObject()).getId();
+                    message = "Payment failed to process.";
+                    status = "declined";
+                    break;
+                case "charge.refunded":
+                    chargeId = ((Charge) event.getData().getObject()).getId();
+                    message = "Payment was refunded.";
+                    status = "refunded";
+                    break;
+                case "charge.pending":
+                    chargeId = ((Charge) event.getData().getObject()).getId();
+                    message = "Payment is pending.";
+                    status = "pending";
+                    break;
+                case "charge.updated":
+                    chargeId = ((Charge) event.getData().getObject()).getId();
+                    message = "This payment was recently updated...";
+                    status = "processing_updated";
+                    break;
+                case "charge.dispute.closed":
+                    chargeId = ((Dispute) event.getData().getObject()).getCharge();
+                    message = "This order was disputed and closed";
+                    status = "disputed_closed";
+                    StripeService.createDispute((Dispute) event.getData().getObject());
+                    break;
+                case "charge.dispute.opened":
+                    chargeId = ((Dispute) event.getData().getObject()).getCharge();
+                    message = "This order is disputed";
+                    status = "disputed_open";
+                    StripeService.createDispute((Dispute) event.getData().getObject());
+                    break;
+                case "charge.dispute.created":
+                    chargeId = ((Dispute) event.getData().getObject()).getCharge();
+                    message = "This order is disputed";
+                    status = "disputed_created";
+                    StripeService.createDispute((Dispute) event.getData().getObject());
+                    break;
+                case "charge.dispute.funds_reinstated":
+                    chargeId = ((Dispute) event.getData().getObject()).getCharge();
+                    message = "Payment Successful/Disputed/Closed";
+                    status = "disputed_closed";
+                    StripeService.createDispute((Dispute) event.getData().getObject());
+                    break;
+                case "charge.dispute.funds_withdrawn":
+                    chargeId = ((Dispute) event.getData().getObject()).getCharge();
+                    message = "Order cancelled/Payment returned";
+                    status = "disputed_closed_lost";
+                    StripeService.createDispute((Dispute) event.getData().getObject());
+                    break;
+                case "charge.dispute.updated":
+                    chargeId = ((Dispute) event.getData().getObject()).getCharge();
+                    message = "This order is disputed";
+                    status = "disputed_updated";
+                    StripeService.createDispute((Dispute) event.getData().getObject());
+                    break;
+                case "charge.refund.updated":
+                    chargeId = ((Refund) event.getData().getObject()).getCharge();
+                    message = "This order was refunded";
+                    status = "refunded";
+                    StripeService.createRefund((Refund) event.getData().getObject());
+                    break;
+                default:
+                    // something went wrong
             }
 
             System.out.println(message);
 
             if (message != null) {
-                stripeService.changeOrderStatus(chargeId, status, message);
+                StripeService.changeOrderStatus(chargeId, status, message);
             }
         } catch(Exception e) {
             e.printStackTrace();
